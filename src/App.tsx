6bedba17c48a76dd95509fa6f125216c5b93a6de
jagadeesh kumar <jagadeesh.kumar@bolt.earth>
2023-11-11 10:51:37 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface UserData {
+  name: {
+    first: string;
+    last: string;
+  };
+  email: string;
 }
 
+const App = () => {
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api');
+      const user = response.data.results[0];
+
+      setUserData({
+        name: {
+          first: user.name.first,
+          last: user.name.last,
+        },
+        email: user.email,
+      });
+      // Save to local storage
+      localStorage.setItem('userData', JSON.stringify(user));
+    } catch (error) {
+      console.error('Error fetching user data', error);
+    }
+  };
+
+  const refreshUserData = () => {
+    setUserData(null); 
+    fetchUserData();
+  };
+
+  useEffect(() => {
+    // Check local storage for existing data
+    const storedData = localStorage.getItem('userData');
+    if (storedData) {
+      setUserData(JSON.parse(storedData));
+    } else {
+      fetchUserData();
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <h3>User Info</h3>
+      {userData ? (
+        <>
+          <p>
+            <strong>Name:</strong>
+             {userData.name.first}  {userData.name.last}
+          </p>
+          <p>
+            <strong>Email:</strong> {userData.email}
+          </p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={refreshUserData}>Refresh</button>
+    </div>
+  );
+};
+
 export default App;
+
